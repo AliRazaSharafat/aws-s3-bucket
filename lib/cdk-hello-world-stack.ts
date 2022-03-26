217ba4069868541a4080f59d75beb86feb1d64fe
EvapDeveloper <aliraza@evap.my>
2022-03-26 16:55:38 +0500

    initialize

--- lib/cdk-hello-world-stack.ts
+++ lib/cdk-hello-world-stack.ts
@@ -1,11 +1,13 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { Stack, StackProps, aws_s3 as s3 } from "aws-cdk-lib";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkHelloWorldStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
+    new s3.Bucket(this, "MyFirstBucket", {
+      versioned: true,
+    });
     // The code that defines your stack goes here
 
     // example resource
